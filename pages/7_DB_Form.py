c619f2b79ecffb4cae1f9d62c9e037256a8c088e
brodie-gray <79341762+brodie-gray@users.noreply.github.com>
2022-07-21 15:31:34 +1000

    Update 7_DB_Form.py

--- pages/7_DB_Form.py
+++ pages/7_DB_Form.py
@@ -184,16 +184,23 @@ def update_screen():
     string_to_see.markdown(f'>{st.session_state["items"][item_i]["sentence"]["S"]}')
     progress_status.markdown(f"#### Progress: {num_labelled} / {NUM_ENTRIES}")
 
-with st.form("name_form",clear_on_submit = False):
-    st.markdown("#### Set Your Name:")
-    name_text = st.empty()
-    name = name_text.text_area(label="Please type your name here")
 
-    # Every form must have a submit button.
-    submitted_name = st.form_submit_button("Set Name")
-    if submitted_name:
-        if not name.isspace():
-            uploader = name 
+placeholder_container = st.empty()
+
+if uploader != 'undefined':
+    with placeholder_container.form("name_form",clear_on_submit = False):
+        placeholder_container.markdown("#### Set Your Name:")
+        name_text = st.empty()
+        name = name_text.text_area(label="Please type your name here")
+
+        # Every form must have a submit button.
+        submitted_name = st.form_submit_button("Set Name")
+        if submitted_name:
+            if not name.isspace():
+                uploader = name 
+                placeholder_container.empty()
+else:
+    st.markdown(f"#### Your Set Name is: {uploader}")
 
 st.markdown("#### Instructions")
 
